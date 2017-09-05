bc97af451ac53d76bbe1d4134c6e54cfcb659a2d
KhalilHemissi <31658873+KhalilHemissi@users.noreply.github.com>
2017-09-05 17:40:30 +0200

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,7 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Content from './Content';
 import Footer from './Footer';*/
-import 'admin-lte/dist/css/AdminLTE.css';
-import 'admin-lte/dist/css/skins/_all-skins.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
-import 'react-bootstrap-table/css/react-bootstrap-table.css'
-
+import * as firebase from "firebase";
 import { browserHistory } from "react-router";
 import { Header, Dashboard, Sidebar } from "react-adminlte-dash";
 
@@ -17,35 +12,34 @@ import { Header, Dashboard, Sidebar } from "react-adminlte-dash";
 
 const navMenu = () => ([
   <Header.Item
-    href={'/Myaccount'}
-    iconClass="fa fa-user-circle"
+    iconClass="fa fa-github"
     key="1"
-    title="My account"
-     profileAction={() => browserHistory.push("/Myaccount")}
-
-    signOutAction={() => {
-      browserHistory.push("/signin");
-      Object.keys(localStorage).forEach(key => {
-        if (key.indexOf('firebase') !== -1) {
-          browserHistory.push("/signin");
-          localStorage.removeItem(key);
-        }
-      })
-    }}
+    title="Github"
   />,
 
 
+
   <Header.UserMenu
-    name="User Name"
-    image="./a.jpg"
+    name="user name"
+    aria-hidden="true"
+    image="./Celtics.png"
     profileAction={() => browserHistory.push("/Myaccount")}
 
     signOutAction={() => {
-      browserHistory.push("/signin");
+              browserHistory.push("/signin");
       Object.keys(localStorage).forEach(key => {
         if (key.indexOf('firebase') !== -1) {
-          browserHistory.push("/signin");
-          localStorage.removeItem(key);
+        firebase.auth().signOut().then(function() {
+          console.log('Signed Out');
+        localStorage.setItem('newname',0)
+        localStorage.setItem('newmail',0)
+        localStorage.setItem('newpass','0')
+        }, function(error) {
+          console.error('Sign Out Error', error);
+        }); 
+        localStorage.removeItem(key);
+
+
         }
       })
     }}
@@ -55,29 +49,34 @@ const navMenu = () => ([
 
 
 const sb = pickTheme => ([
- 
- 
-  <Sidebar.Menu header="NAVIGATION PRINCIPALE" key="3">
+  <Sidebar.UserPanel
+    name="Put username here"
+    image="./Celtics.png"
+    online
+    key="1"
+  />,
+  <Sidebar.Search key="2" />,
+  <Sidebar.Menu header="MENU PRINCIPALE" key="3">
     <Sidebar.Menu.Item icon={{ className: 'fa-dashboard' }} title="Acceuil" />
-    <Sidebar.Menu.Item icon={{ className: 'fa-user-circle' }} title="Votre Acceuil" href="/Myaccount" />
-    <Sidebar.Menu.Item icon={{ className: 'fa-file' }} title="Gestion des fiches" onClick={() => { browserHistory.push("/addFile")}} /> 
-    <Sidebar.Menu.Item icon={{ className: 'fa-user-md' }} title="Gestion des medecins" href="/addDoctor" />
+    <Sidebar.Menu.Item icon={{ className: 'fa-user-circle' }} title="Votre Acceuil" onClick={() => browserHistory.push("/Myaccount")}/>
+    <Sidebar.Menu.Item icon={{ className: 'fa-file' }} title="Gestion des fiches" onClick={() => browserHistory.push("/AddFile")} />
+    <Sidebar.Menu.Item icon={{ className: 'fa-user-md' }} title="Gestion des medecins" onClick={() => browserHistory.push("/addDoctor")} />
     <Sidebar.Menu.Item icon={{ className: 'fa-envelope' }} title="Gestion des alertes" href="/addAlert" />
     <Sidebar.Menu.Item icon={{ className: 'fa-dashboard' }} title="Historiques des alertes" href="/checkhistory" />
-    <Sidebar.Menu.Item icon={{ className: 'fa-pie-chart' }} title="Analytics" href="" />
+    <Sidebar.Menu.Item icon={{ className: 'fa-pie-chart' }} title="Analytics" href="/analytics"/>
     <Sidebar.Menu.Item icon={{ className: 'fa-dashboard' }} title="Support" href="/help" />
   </Sidebar.Menu>,
 ]);
 
 
 const footer = () => ([
-  <strong>
+  <strong key="copyright">
     <span>Copyright © 2017-2018 </span>
     <a href="https://katomi.co/">Katomi</a>
     <span>. </span>
   </strong>,
-  <span> All rights reserved.</span>,
-  <div style={{ float: 'right' }}>
+  <span key="allrights"> All rights reserved.</span>,
+  <div key="version" style={{ float: 'right' }}>
     <b>Version</b>
     <span> 0.1</span>
   </div>,
@@ -93,11 +92,12 @@ const App = ({ children, theme, pickTheme }) => (
     footerChildren={footer()}
     theme={theme}
   >
-    <div style={{ height: "150vh" }} id="children">
+    <div style={{ height: "100vh" }} id="children">
       {children}
     </div>
-  </Dashboard>
 
+
+  </Dashboard>
 );
 
 export default App;
